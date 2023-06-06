bfcec95a46aab01485dc434230761b0c75e5540e
Juan Ferrero <jpf13dw@gmail.com>
2023-06-05 22:12:05 -0300

    3er PreEntrega

--- JavaScript-43075/main.js
+++ JavaScript-43075/main.js
@@ -1,28 +1,29 @@
 let titulo = document.getElementById("title");
 titulo.className = "titulo";
 
-// // Constructor de productos
+// Constructor de productos
 
 class Zapatillas {
-    constructor(objeto, marca, precio) {
+    constructor(id, objeto, marca, precio) {
+        this.id = id;
         this.objeto = objeto;
         this.marca = marca;
         this.precio = precio;
     }
 }
 
-const zapatilla1 = new Zapatillas("./img/jordan1.webp", "Jordan Delta 3 Low", 55000);
-const zapatilla2 = new Zapatillas("./img/jordan2.webp", "Air Jordan 5 Mix", 45000);
-const zapatilla3 = new Zapatillas("./img/jordan3.webp", "Jordan Air 200E", 40000);
-const zapatilla4 = new Zapatillas("./img/jordan4.webp", "Jordan Legacy 312 Low", 50000);
-const zapatilla5 = new Zapatillas("./img/jordan5.webp", "Air Jordan 1 KO", 35000);
-const zapatilla6 = new Zapatillas("./img/jordan6.webp", "Air Jordan 11", 55000);
-const zapatilla7 = new Zapatillas("./img/jordan7.webp", "Air Jordan XXXVII", 45000);
-const zapatilla8 = new Zapatillas("./img/jordan8.webp", "Jumpman Two Trey", 65000);
-const zapatilla9 = new Zapatillas("./img/jordan9.webp", "Jordan Series Mind", 35000);
-const zapatilla10 = new Zapatillas("./img/jordan10.webp", "Air Jordan 12", 60000);
-const zapatilla11 = new Zapatillas("./img/jordan11.webp", "Air Jordan Max", 65000);
-const zapatilla12 = new Zapatillas("./img/jordan12.webp", "Air Jordan 4 Midnight ", 55000);
+const zapatilla1 = new Zapatillas(1, "./img/jordan1.webp", "Jordan Delta 3 Low", 55000);
+const zapatilla2 = new Zapatillas(2, "./img/jordan2.webp", "Air Jordan 5 Mix", 45000);
+const zapatilla3 = new Zapatillas(3, "./img/jordan3.webp", "Jordan Air 200E", 40000);
+const zapatilla4 = new Zapatillas(4, "./img/jordan4.webp", "Jordan Legacy 312 Low", 50000);
+const zapatilla5 = new Zapatillas(5, "./img/jordan5.webp", "Air Jordan 1 KO", 35000);
+const zapatilla6 = new Zapatillas(6, "./img/jordan6.webp", "Air Jordan 11", 55000);
+const zapatilla7 = new Zapatillas(7, "./img/jordan7.webp", "Air Jordan XXXVII", 45000);
+const zapatilla8 = new Zapatillas(8, "./img/jordan8.webp", "Jumpman Two Trey", 65000);
+const zapatilla9 = new Zapatillas(9, "./img/jordan9.webp", "Jordan Series Mind", 35000);
+const zapatilla10 = new Zapatillas(10, "./img/jordan10.webp", "Air Jordan 12", 60000);
+const zapatilla11 = new Zapatillas(11, "./img/jordan11.webp", "Air Jordan Max", 65000);
+const zapatilla12 = new Zapatillas(12, "./img/jordan12.webp", "Air Jordan 4 Midnight ", 55000);
 
 const arrayZapatillas = [];
 
@@ -39,28 +40,32 @@ arrayZapatillas.push(zapatilla10);
 arrayZapatillas.push(zapatilla11);
 arrayZapatillas.push(zapatilla12);
 
+// Recorro el array de productos y los agrego al carrito
+
+let contenedor = document.getElementById("container");
+contenedor.className = "container";
 
 arrayZapatillas.forEach((item) => {
     let div = document.createElement("div");
+    div.classList.add('productos');
     div.innerHTML = `
     <img src="${item.objeto}" alt="" width="250px">
     <p>${item.marca}</p>
     <b>$${item.precio}</b>
     <br>
-    <button id="boton" class="productos-button">COMPRAR</button>
+    <button id="boton${item.id}" class="productos-button">COMPRAR</button>
     `;
     
-    container.append(div);
-    div.classList.add('productos');
+    contenedor.appendChild(div);
     
-    let boton = document.getElementById("boton");
+    let boton = document.getElementById(`boton${item.id}`);
 
     boton.addEventListener("click", () => { 
-        console.log("Agregar al Carrrito");
+        console.log(`Agregar al carrito "ID:${item.id}" ${item.marca}`);
     })
 
 })
 
-let contenedor = document.getElementById("container");
-contenedor.className = "container";
+
+localStorage.setItem("arrayZapatillas", JSON.stringify(arrayZapatillas));
 
